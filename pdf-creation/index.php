db823e38d72c60215ef2dd86f8db8f0454feba33
sadguruswamymalladi <sadguruswamymalladi@gmail.com>
2023-04-18 13:20:15 +0530

    pdf creation

--- pdf-creation/index.php
+++ pdf-creation/index.php
@@ -1,8 +1,9 @@
 
-<?php  // codeingiter
+<?php  // codeingiter pdf creation
+$html = $this->create_invoice_data($sale_id,$seller_id,2);
 $this->load->library('Pdf');
-$html = '<h1>Helllo world</h1>';
 $this->dompdf->loadHtml($html);
-$this->dompdf->setPaper('A4', 'landscape');
+$this->dompdf->setPaper('A4', 'Landscape');
 $this->dompdf->render();
-$this->dompdf->stream("welcome.pdf");
+$this->dompdf->stream("invoice-".$sno.".pdf");
+//redirect(current_url());
